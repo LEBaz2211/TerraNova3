eba86ad9772951bc1367c86e0388d69bbad25cd5
LEBaz2211 <92997786+LEBaz2211@users.noreply.github.com>
2022-12-30 23:29:02 +0100

    models

---
+++ Models/AbstractKiller.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace Terranova3.Models;
+
+internal abstract class AbstractKiller : AbstractMoving
+{
+    // The amount of damage that the killer can inflict on its prey
+    protected int attackDamage;
+
+    public AbstractKiller(int startX, int startY, int maxEnergy, int maxHitPoints,string entityType, int speed, int visionRadius, int movementRadius, int attackDamage, int attackRadius)
+        : base( startX, startY, maxEnergy, maxHitPoints, entityType, speed, visionRadius, movementRadius)
+    {
+        this.attackDamage = attackDamage;
+    }
+
+    // Returns the amount of damage that the killer can inflict on its prey
+    public int GetAttackDamage()
+    {
+        return attackDamage;
+    }
+
+    // Abstract method that must be implemented by derived classes
+    public abstract void Attack(Entity prey);
+}
+
---
+++ Models/Predator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terranova3.Models;
+
+internal class Predator : AbstractKiller
+{
+
+    public Predator(int startX, int startY, int maxEnergy, int maxHitPoints, string entityType, int speed, int visionRadius, int movementRadius, int attackDamage) : base(startX, startY, maxEnergy, maxHitPoints, entityType, speed, visionRadius, movementRadius, attackDamage)
+    {
+    }
+
+    // Returns the attack strength of the predator
+    public int GetAttackStrength()
+    {
+        return attackStrength;
+    }
+
+    // Returns the range at which the predator can detect prey
+    public int GetVisionRange()
+    {
+        return visionRange;
+    }
+
+    // Returns the range at which the predator can attack prey
+    public int GetAttackRange()
+    {
+        return attackRange;
+    }
+
+    // Sets the current amount of hit points that the predator has
+    public void SetHitPoints(int hitPoints)
+    {
+        this.hitPoints = hitPoints;
+    }
+
+    // Sets the attack strength of the predator
+    public void SetAttackStrength(int attackStrength)
+    {
+        this.attackStrength = attackStrength;
+    }
+
+    // Sets the movement speed of the predator
+    public void SetMovementSpeed(int movementSpeed)
+    {
+        this.movementSpeed = movementSpeed;
+    }
+
+    // Sets the range at which
+
