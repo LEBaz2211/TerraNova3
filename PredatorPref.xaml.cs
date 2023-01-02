99bd02f0ad3e7a8ec7e340f9769e50749a493b64
ChillinSoul <99248410+ChillinSoul@users.noreply.github.com>
2023-01-02 13:58:03 +0100

    predator pref ui done

--- PredatorPref.xaml.cs
+++ PredatorPref.xaml.cs
@@ -6,15 +6,17 @@ public partial class PredatorPref : ContentPage
     public int Energy { get; set; }
     public int EnergyDecayPercentage { get; set; }
     public int MatingEnergyCostPercentage { get; set; }
-    public int BreedingCooldown { get; set; }
     public int GestationPeriod { get; set; }
+
     public int AttackDamage { get; set; }
+    public int AttackRadius { get; set; }
     public int VisionRadius { get; set; }
     public int ContactRadius { get; set; }
 
     public PredatorPref()
     {
         InitializeComponent();
+        SetPref();
 
 
     }
@@ -30,6 +32,7 @@ public partial class PredatorPref : ContentPage
     private void OnResetClicked(object sender, EventArgs e)
     {
         ClearPref();
+        SetPref();
     }
     public void Parse()
     {
@@ -37,9 +40,10 @@ public partial class PredatorPref : ContentPage
         Energy = int.Parse(energyEntry.Text);
         EnergyDecayPercentage = int.Parse(energyDecayEntry.Text);
         MatingEnergyCostPercentage = int.Parse(matingEnergyCostEntry.Text);
-        BreedingCooldown = int.Parse(breedingCooldownEntry.Text);
+
         GestationPeriod = int.Parse(gestationPeriodEntry.Text);
         AttackDamage = int.Parse(attackDamageEntry.Text);
+        AttackRadius = int.Parse(attackRadiusEntry.Text);
         VisionRadius = int.Parse(visionRadiusEntry.Text);
         ContactRadius = int.Parse(contactRadiusEntry.Text);
     }
@@ -51,11 +55,24 @@ public partial class PredatorPref : ContentPage
         Preferences.Set("PredatorEnergy", Energy);
         Preferences.Set("PredatorEnergyDecayPercentage", EnergyDecayPercentage);
         Preferences.Set("PredatorMatingEnergyCostPercentage", MatingEnergyCostPercentage);
-        Preferences.Set("PredatorBreedingCooldown", BreedingCooldown);
+
         Preferences.Set("PredatorGestationPeriod", GestationPeriod);
         Preferences.Set("PredatorAttackDamage", AttackDamage);
+        Preferences.Set("PredatorAttackRadius", AttackRadius);
         Preferences.Set("PredatorVisionRadius", VisionRadius);
         Preferences.Set("PredatorContactRadius", ContactRadius);
+    }
+    public void SetPref()
+    {
+        hitPointsEntry.Text = $"{Preferences.Get("PredatorHitPoints", 100)}";
+        energyEntry.Text = $"{Preferences.Get("PredatorEnergy", 1000)}";
+        energyDecayEntry.Text = $"{Preferences.Get("PredatorEnergyDecayPercentage", 1)}";
+        matingEnergyCostEntry.Text = $"{Preferences.Get("PredatorMatingEnergyCostPercentage", 50)}";
+        gestationPeriodEntry.Text = $"{Preferences.Get("PredatorGestationPeriod", 50)}";
+        attackDamageEntry.Text  = $"{Preferences.Get("PredatorAttackDamage", 30)}";
+        attackRadiusEntry.Text = $"{Preferences.Get("PredatorAttackRadius", 1)}";
+        visionRadiusEntry.Text = $"{Preferences.Get("PredatorVisionRadius", 10)}";
+        contactRadiusEntry.Text = $"{Preferences.Get("PredatorContactRadius", 0)}";
     }
     public static void ClearPref()
     {
@@ -63,11 +80,13 @@ public partial class PredatorPref : ContentPage
         Preferences.Remove("PredatorEnergy");
         Preferences.Remove("PredatorEnergyDecayPercentage");
         Preferences.Remove("PredatorMatingEnergyCostPercentage");
-        Preferences.Remove("PredatorBreedingCooldown");
+
         Preferences.Remove("PredatorGestationPeriod");
         Preferences.Remove("PredatorAttackDamage");
+        Preferences.Remove("PredatorAttackRadius");
         Preferences.Remove("PredatorVisionRadius");
         Preferences.Remove("PredatorContactRadius");
+        
     }
 }
 
